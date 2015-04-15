256db1c2e84050a58735fbf51c2256f37c70b435
Kerstin <kerstinfinke@hotmail.com>
2015-04-15 10:32:35 +0200

    [BUGFIX javascript isotope start when document.ready]

--- Resources/Public/JavaScript/isotope-custom.js
+++ Resources/Public/JavaScript/isotope-custom.js
@@ -4,20 +4,21 @@
 
 /* Requires isotope.pkgd.min.js & imagesloaded.pkgd.min.js */
 var jQuery = $;
-/**
- * Isotope filtering
- */
-// init Isotope
-var $container = jQuery('#isotope-container').imagesLoaded( function() {
-  $container.isotope({
-    itemSelector: '.isotope-item',
-    layoutMode: 'fitRows'
-  });
-});
+
 
 // filter items on button click
 
 jQuery(document).ready(function() {
+    /**
+     * Isotope filtering
+     */
+// init Isotope
+    var $container = jQuery('#isotope-container').imagesLoaded( function() {
+        $container.isotope({
+            itemSelector: '.isotope-item',
+            layoutMode: 'fitRows'
+        });
+    });
     jQuery('#filters a').on('click', function() {
         var filterValue = jQuery(this).attr('data-filter');
         $container.isotope({filter: filterValue});
